56f045bc75c8cbd50ee959c5fc5730de57e06ea1
Nas <nassersanou23@gmail.com>
2024-05-19 22:55:46 +0200

    adds locsl regions [2]

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,22 +18,22 @@ async function getRegionMap() {
     regionMapUpdated < Date.now() - 3600 * 1000
   ) {
     // Fetch regions from Medusa. We can't use the JS client here because middleware is running on Edge and the client needs a Node environment.
-    // const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
-    //   next: {
-    //     revalidate: 3600,
-    //     tags: ["regions"],
-    //   },
-    // }).then((res) => res.json())
-    const  regions = [
-      {"id":"reg_01HY70E5X9C0AY6C5GH984V6YR","created_at":"2024-05-18T23:26:20.516Z","updated_at":"2024-05-18T23:26:20.516Z","deleted_at":null,"name":"EU","currency_code":"eur","tax_rate":0,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":215,"iso_2":"se","iso_3":"swe","num_code":752,"name":"SWEDEN","display_name":"Sweden","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},
-      {"id":235,"iso_2":"gb","iso_3":"gbr","num_code":826,"name":"UNITED KINGDOM","display_name":"United Kingdom","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":82,"iso_2":"de","iso_3":"deu","num_code":276,"name":"GERMANY","display_name":"Germany","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":60,"iso_2":"dk","iso_3":"dnk","num_code":208,"name":"DENMARK","display_name":"Denmark","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},
-      {"id":75,"iso_2":"fr","iso_3":"fra","num_code":250,"name":"FRANCE","display_name":"France","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":209,"iso_2":"es","iso_3":"esp","num_code":724,"name":"SPAIN","display_name":"Spain","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":109,"iso_2":"it","iso_3":"ita","num_code":380,"name":"ITALY","display_name":"Italy","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]},{"id":"reg_01HY91AP7442NKFDAW7WSDJYS7","created_at":"2024-05-19T18:20:23.558Z","updated_at":"2024-05-19T18:20:23.558Z","deleted_at":null,"name":"MALAWI","currency_code":"usd","tax_rate":1,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":134,"iso_2":"mw","iso_3":"mwi","num_code":454,"name":"MALAWI","display_name":"Malawi","region_id":"reg_01HY91AP7442NKFDAW7WSDJYS7"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]},{"id":"reg_01HY70E5Y8625YRMSSKBT74G11","created_at":"2024-05-18T23:26:20.516Z","updated_at":"2024-05-18T23:26:20.516Z","deleted_at":null,"name":"NA","currency_code":"usd","tax_rate":0,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":236,"iso_2":"us","iso_3":"usa","num_code":840,"name":"UNITED STATES","display_name":"United States","region_id":"reg_01HY70E5Y8625YRMSSKBT74G11"},{"id":39,"iso_2":"ca","iso_3":"can","num_code":124,"name":"CANADA","display_name":"Canada","region_id":"reg_01HY70E5Y8625YRMSSKBT74G11"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]}
-  ]
+    const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
+      next: {
+        revalidate: 3600,
+        tags: ["regions"],
+      },
+    }).then((res) => res.json())
+  //   const  regions = [
+  //     {"id":"reg_01HY70E5X9C0AY6C5GH984V6YR","created_at":"2024-05-18T23:26:20.516Z","updated_at":"2024-05-18T23:26:20.516Z","deleted_at":null,"name":"EU","currency_code":"eur","tax_rate":0,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":215,"iso_2":"se","iso_3":"swe","num_code":752,"name":"SWEDEN","display_name":"Sweden","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},
+  //     {"id":235,"iso_2":"gb","iso_3":"gbr","num_code":826,"name":"UNITED KINGDOM","display_name":"United Kingdom","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":82,"iso_2":"de","iso_3":"deu","num_code":276,"name":"GERMANY","display_name":"Germany","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":60,"iso_2":"dk","iso_3":"dnk","num_code":208,"name":"DENMARK","display_name":"Denmark","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},
+  //     {"id":75,"iso_2":"fr","iso_3":"fra","num_code":250,"name":"FRANCE","display_name":"France","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":209,"iso_2":"es","iso_3":"esp","num_code":724,"name":"SPAIN","display_name":"Spain","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"},{"id":109,"iso_2":"it","iso_3":"ita","num_code":380,"name":"ITALY","display_name":"Italy","region_id":"reg_01HY70E5X9C0AY6C5GH984V6YR"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]},{"id":"reg_01HY91AP7442NKFDAW7WSDJYS7","created_at":"2024-05-19T18:20:23.558Z","updated_at":"2024-05-19T18:20:23.558Z","deleted_at":null,"name":"MALAWI","currency_code":"usd","tax_rate":1,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":134,"iso_2":"mw","iso_3":"mwi","num_code":454,"name":"MALAWI","display_name":"Malawi","region_id":"reg_01HY91AP7442NKFDAW7WSDJYS7"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]},{"id":"reg_01HY70E5Y8625YRMSSKBT74G11","created_at":"2024-05-18T23:26:20.516Z","updated_at":"2024-05-18T23:26:20.516Z","deleted_at":null,"name":"NA","currency_code":"usd","tax_rate":0,"tax_code":null,"gift_cards_taxable":true,"automatic_taxes":true,"tax_provider_id":null,"metadata":null,"countries":[{"id":236,"iso_2":"us","iso_3":"usa","num_code":840,"name":"UNITED STATES","display_name":"United States","region_id":"reg_01HY70E5Y8625YRMSSKBT74G11"},{"id":39,"iso_2":"ca","iso_3":"can","num_code":124,"name":"CANADA","display_name":"Canada","region_id":"reg_01HY70E5Y8625YRMSSKBT74G11"}],"payment_providers":[{"id":"manual","is_installed":true}],"fulfillment_providers":[{"id":"manual","is_installed":true}]}
+  // ]
     
     if (!regions) {
       notFound()
     }
-
+  //  const parsed = JSON.parse(regions)
     // Create a map of country codes to regions.
     regions.forEach((region: Region) => {
       region.countries.forEach((c) => {
