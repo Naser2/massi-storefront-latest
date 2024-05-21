cacaacebd1ddc95cb2f6a6d2879c93445ac0e8b3
Nas <nassersanou23@gmail.com>
2024-05-21 05:12:46 +0200

    Testing Fetch Categories from Admin

--- src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -16,15 +16,18 @@ type Props = {
 export async function generateStaticParams() {
   const product_categories = await listCategories()
 
-  if (product_categories ==null) {
+  if (!product_categories) {
+    console.log("CATEGORIES HANDLES === NULL? ", product_categories)
     return []
-  }
+  } 
 
   const countryCodes = await listRegions().then((regions) =>
     regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
   )
 
-  const categoryHandles = product_categories?.map((category) => category.handle)
+ const categoryHandles = product_categories.map((category) => category.handle)
+
+ console.log("CATEGORIES HANDLES", product_categories)
 
   const staticParams = countryCodes
     ?.map((countryCode) =>
